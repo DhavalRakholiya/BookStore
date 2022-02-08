c41273a5e6057a9008da40deacc54bdc5a214c9c
Dhaval Rakholiya <dhavalrakholiya5@gmail.com>
2022-02-08 16:09:22 +0530

    Environment Variables

--- MyBookStore/MyBookStore/Startup.cs
+++ MyBookStore/MyBookStore/Startup.cs
@@ -26,59 +26,34 @@ namespace MyBookStore
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Use(async (context, next) =>
-            {
-                await context.Response.WriteAsync("Hello from first custom middleware");
-
-                await next();
-
-                await context.Response.WriteAsync("Hello from first-second custom middleware");
-            });
-
-            app.Use(async (context, next) =>
-            {
-                await context.Response.WriteAsync("Hello from second-first custom middleware");
-
-                await next();
-
-                await context.Response.WriteAsync("Hello from second-second custom middleware");
-            });
-
-            app.Use(async (context, next) =>
-            {
-                await context.Response.WriteAsync("Hello from third-first custom middleware");
-
-                await next();
-            });
-
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    if (env.IsDevelopment())
+                    {
+                        await context.Response.WriteAsync("Hello From Development Server");
+                    }
+                    else if (env.IsProduction())
+                    {
+                        await context.Response.WriteAsync("Hello From Production Server");
+                    }
+                    else if (env.IsStaging())
+                    {
+                        await context.Response.WriteAsync("Hello From Staging Server");
+                    }
+                    else if (env.IsEnvironment("Develop"))
+                    {
+                        await context.Response.WriteAsync("Hello From Custom Server");
+                    }
+                    else
+                    {
+                        await context.Response.WriteAsync(env.EnvironmentName);
+                    }
                 });
             });
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/dhaval", async context =>
-                {
-                    await context.Response.WriteAsync("Hello Dhaval!");
-                });
-            });
-
-            //We can also use Map insted of MapGet 
-            //difference is that MapGet can only handle get requests while Map can handle all type of requests
-
-            //app.UseEndpoints(endpoints =>
-            //{
-            //    endpoints.Map("/dhaval", async context =>
-            //    {
-            //        await context.Response.WriteAsync("Hello Vikas!");
-            //    });
-            //});
         }
     }
 }
