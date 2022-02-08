1d6f38230e8b27026981043e309cb7da92cf3844
Dhaval Rakholiya <dhavalrakholiya5@gmail.com>
2022-02-08 17:26:28 +0530

    Controller

---
+++ MyBookStore/MyBookStore/Controllers/BookController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyBookStore.Controllers
+{
+    public class BookController : Controller
+    {
+        public string GetAllBooks()
+        {
+            return "All Books";
+        }
+
+        public string GetBookById(int id)
+        {
+            return $"Book With Id: {id}";
+        }
+
+        public string SearchBooks(string bookName, string authorName)
+        {
+            return $"Book with Name: {bookName} & Author: {authorName}";
+        }
+    }
+}
