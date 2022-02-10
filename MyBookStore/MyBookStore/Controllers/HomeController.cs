63f006b3445a32969bae93890002a1d4364fa776
Dhaval Rakholiya <dhavalrakholiya5@gmail.com>
2022-02-10 10:38:00 +0530

    ViewBag in Asp.Net Core MVC

--- MyBookStore/MyBookStore/Controllers/HomeController.cs
+++ MyBookStore/MyBookStore/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Dynamic;
+using MyBookStore.Models;
 
 namespace MyBookStore.Controllers
 {
@@ -10,7 +12,17 @@ namespace MyBookStore.Controllers
     {
         public ViewResult Index()
         {
+            ViewBag.Title = 123;
+
+            dynamic data = new ExpandoObject();
+            data.id = 1;
+            data.name = "Bhaumik";
+            ViewBag.Obj = data;
+
+            ViewBag.Type = new BookModel() { Id = 1, Author = "Dhaval" };
             return View();
+
+            
             //return View("AboutUs");
             //Full Path
             //return View("TempView/Index.cshtml");
