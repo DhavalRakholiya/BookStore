277402ae0194c47280483758974ecf6866152a4f
Dhaval Rakholiya <dhavalrakholiya5@gmail.com>
2022-02-10 16:26:17 +0530

    Added new form to add a new book

--- MyBookStore/MyBookStore/Controllers/BookController.cs
+++ MyBookStore/MyBookStore/Controllers/BookController.cs
@@ -33,5 +33,16 @@ namespace MyBookStore.Controllers
         {
             return _bookRepository.SearchBook(bookName, authorName);
         }
+
+        public ViewResult AddNewBook()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult AddNewBook(BookModel bookModel)
+        {
+            return View();
+        }
     }
 }
