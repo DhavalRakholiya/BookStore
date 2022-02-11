a2fd01c90e16ff6c184cc2f21cba3b983ca66b25
Dhaval Rakholiya <dhavalrakholiya5@gmail.com>
2022-02-11 17:45:57 +0530

    Create group in asp.net core dropdown | SelectListGroup

--- MyBookStore/MyBookStore/Controllers/BookController.cs
+++ MyBookStore/MyBookStore/Controllers/BookController.cs
@@ -37,13 +37,19 @@ namespace MyBookStore.Controllers
 
         public ViewResult AddNewBook(bool isSuccess = false, int bookId = 0)
         {
+            var group1 = new SelectListGroup() { Name = "Group 1" };
+            var group2 = new SelectListGroup() { Name = "Group 2", Disabled = true };
+            var group3 = new SelectListGroup() { Name = "Group 3" };
+
             //ViewBag.languageList = new SelectList(GetLanguage(), "Id", "Text", "2");
             ViewBag.languageList = new List<SelectListItem>()
             {
-                new SelectListItem(){Text = "Hindi", Value = "1" },
-                new SelectListItem(){Text = "English", Value = "2", Disabled = true },
-                new SelectListItem(){Text = "Dutch", Value = "3", Selected = true },
-                new SelectListItem(){Text = "Tamil", Value = "4", Disabled = true },
+                new SelectListItem(){Text = "Hindi", Value = "1", Group = group1 },
+                new SelectListItem(){Text = "English", Value = "2", Group = group1 },
+                new SelectListItem(){Text = "Dutch", Value = "3", Group = group2},
+                new SelectListItem(){Text = "Tamil", Value = "4", Group = group2 },
+                new SelectListItem(){Text = "Urdu", Value = "5" , Group = group3},
+                new SelectListItem(){Text = "Chinese", Value = "6", Group = group3 }
             };
             ViewBag.isSuccess = isSuccess;
             ViewBag.BookId = bookId;
