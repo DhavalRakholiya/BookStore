ecd0156956516fc160a7c3c39ac9ff6177a5d90b
Dhaval Rakholiya <dhavalrakholiya5@gmail.com>
2022-02-17 17:12:54 +0530

    Override existing html tag in asp.net core

---
+++ MyBookStore/MyBookStore/Helpers/BigTagHelper.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyBookStore.Helpers
+{
+    [HtmlTargetElement("Big")]
+    [HtmlTargetElement(Attributes = "Big")]
+    public class BigTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "h3";
+            output.Attributes.RemoveAll("Big");
+            output.PreContent.SetHtmlContent("<p>Pre Content</p>");
+            output.PostContent.SetHtmlContent("<p>Post Content</p>");
+            output.Attributes.SetAttribute("class", "h3");
+        }
+    }
+}
